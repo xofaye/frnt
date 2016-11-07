7b5e477583cd385e8f9ba9e582ba7b8b28114900
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-11-07 13:32:31 -0500

    Models: FnrtUser + Location

--- Backend/frnt/main/models.py
+++ Backend/frnt/main/models.py
@@ -1,3 +1,31 @@
+import os
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
+class Location(models.Model):
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=15)
+    street_address = models.CharField(max_length=100)
+
+class FnrtUser(models.Model):
+    '''
+    User:
+        username
+        password
+        first_name
+        last_name
+        email
+    '''
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    location = models.OneToOneField('Location')
+    profile_image = models.ImageField(upload_to=get_image_path, blank=True)
+    biography = models.CharField(max_length=500, blank=True)
+
+    rating = models.DecimalField(default=0, decimal_places=2)
+    rating_count = models.IntegerField(default=0)
