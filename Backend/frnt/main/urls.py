1a785bd6616994eaa1d70afaaefed535644072ab
xofaye <faye.feng26@gmail.com>
2016-11-12 11:56:25 -0500

    Completed view_profile

--- Backend/frnt/main/urls.py
+++ Backend/frnt/main/urls.py
@@ -6,12 +6,14 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^register/$', views.register_user, name='register'),
-    #url(r'^accounts/login/$', auth_views.login),
 
     url('^', include('django.contrib.auth.urls')),
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^search/$', views.search_furniture, name='search'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
     url(r'^logout_success/$', views.logout_success, name='logout_success'),
+    url(r'^(?P<username>[\w]+)/', include([
+        url(r'^$', views.view_profile, name='view_profile'),
+    ])),
 
 ]
