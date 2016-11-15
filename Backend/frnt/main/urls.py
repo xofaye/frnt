58c7eefa67408307ef7528847ae2ea049e7a62c2
Hanchen Wang <hanchen.wang@mail.utoronto.ca>
2016-11-15 13:01:35 -0500

    urls: posting Add & Edit

--- Backend/frnt/main/urls.py
+++ Backend/frnt/main/urls.py
@@ -5,18 +5,21 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^register/$', views.register_user, name='register'),
-
     url('^', include('django.contrib.auth.urls')),
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
-    url(r'^search/$', views.search_furniture, name='search'),
+
+    url(r'^register/$', views.register_user, name='register'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
-    url(r'^logout_success/$', views.logout_success, name='logout_success'),
     url(r'^(?P<username>[\w]+)/', include([
         url(r'^$', views.view_profile, name='view_profile'),
     ])),
-        url(r'^listing/(?P<id>[\w]+)/', include([
+    url(r'^logout_success/$', views.logout_success, name='logout_success'),
+    
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^search/$', views.search_furniture, name='search'),
+    url(r'^listing/(?P<id>[\w]+)/', include([
         url(r'^$', views.view_listing, name='view_listing'),
     ])),
 
+    url(r'^posting/add$', views.add_posting, name='add_posting'),
+    url(r'^posting/edit$', views.edit_posting, name='edit_posting'),
 ]
